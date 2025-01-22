a72aad969435fef082542e7e05a9e0f97ed561f1
truehd24 <harrisondaniell24@gmail.com>
2025-01-21 19:34:11 -0500

    Temp portfolio

---
+++ main.js
@@ -0,0 +1,22 @@
+function playGif(element, gifSrc) {
+    element.src = gifSrc;
+}
+
+function stopGif(element, staticSrc) {
+    element.src = staticSrc;
+}
+window.onscroll = function() {
+const button = document.querySelector('.back-to-top');
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+button.classList.add('visible');
+} else {
+button.classList.remove('visible');
+}
+};
+
+function scrollToTop() {
+window.scrollTo({
+top: 0,
+behavior: 'smooth'
+});
+}
